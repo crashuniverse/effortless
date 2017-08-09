a17c034ee155c7165fe901e7ac9b918b6e311c56
Priya Ranjan Singh <mail@priyaranjan.net>
2017-08-10 01:02:58 +0530

    modify navigation mechanism to take a longer but reliable path to efforts

--- tests/efforts.js
+++ tests/efforts.js
@@ -1,20 +1,27 @@
 this.efforts = function (browser) {
   var username = process.env.USERNAME;
   var password = process.env.PASSWORD;
+  var SHORT_INTERVAL = 5000;
+  var LONG_INTERVAL = 10000;
+  var REALLY_LONG_INTERVAL = 20000;
+
   browser.url('http://mywipro.wipro.com');
   browser.deleteCookies();
   browser.click('.idp');
-  browser.waitForElementVisible('#userNameInput', 5000);
+  browser.waitForElementVisible('#userNameInput', SHORT_INTERVAL);
   browser.setValue('#userNameInput', 'wipro\\' + username);
   browser.setValue('#passwordInput', password);
   browser.click('#submitButton');
-  browser.waitForElementVisible('#Find_an_App', 5000);
-  browser.click('#Find_an_App');
-  browser.waitForElementVisible('#findAppTxtId', 5000);
-  browser.setValue('#findAppTxtId', 'myefforts');
-  browser.click('#btnSearch');
-  browser.pause(10000);
+  browser.waitForElementVisible('.appstore_btn', SHORT_INTERVAL);
+  browser.click('.appstore_btn');
+  browser.waitForElementVisible('.MYTIME', SHORT_INTERVAL);
+  browser.click('.MYTIME');
+  browser.maximizeWindow();
   browser.frame('divMainmyWiproPortalViewFrame');
+  browser.waitForElementVisible('#tmsMobileId', REALLY_LONG_INTERVAL);
+  browser.click('#tmsMobileId');
+  browser.waitForElementVisible('#mainContent', LONG_INTERVAL);
+  browser.pause(LONG_INTERVAL);
   browser.setValue('input[id="0_0_4_0"]', '8');
   // browser.setValue('input[id="1_0_4_0"]', '8');
   // browser.setValue('input[id="2_0_4_0"]', '8');
@@ -23,7 +30,7 @@ this.efforts = function (browser) {
   browser.click('#submitdata');
 
   // observe: for humans
-  browser.pause(3000);
+  browser.pause(SHORT_INTERVAL);
 
   // end the session
   browser.end();
